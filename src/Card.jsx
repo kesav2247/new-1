4e73b1a94950e329927221cc8f192de9bf0901ab
kesav2247 <kesavankishore3@gmail.com>
2023-06-30 23:52:42 +0530

    this is new

---
+++ src/Card.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+function Card() {
+    return (
+        <div className="main">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4 ">
+                    <div className="box-inside">
+                        
+                            <h5 className="text-center text-muted" >FREE</h5>
+                            <h6 className="text-center"><span className="amount">$0</span>/month</h6>
+                            <hr />
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Single User</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;5GB Storage</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Unlimited Public Projects</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Community Access</div>
+                            <div className="text-muted"> <i class="fas fa-times fa-lg"></i> &nbsp; Unlimited Private Projects</div>
+                            <div className="text-muted"> <i class="fas fa-times fa-lg"></i> &nbsp; Dedicated Phone Support</div>
+                            <div className="text-muted"> <i class="fas fa-times fa-lg"></i> &nbsp; Free Subdomainr</div>
+                            <div className="text-muted"> <i class="fas fa-times fa-lg"></i> &nbsp; Monthly Status Reports</div>
+                            <div className="prycing">
+                                <button type="button" className="btn btn-primary">BUTTON</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    
+                
+
+
+
+                <div className="col-md-4 items">
+                    <div className="box-inside">
+                       
+                            <h5 className="text-center text-muted" >PLUS</h5>
+                            <h6 className="text-center"><span className="amount">$9</span>/month</h6>
+                            <hr />
+                            <div> <i class="fas fa-check fa-lg"></i>&nbsp;<strong> 5 Users</strong></div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;50GB Storagee</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Unlimited Public Projects</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Community Access</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Unlimited Private Projects</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Dedicated Phone Support</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Free Subdomain</div>
+                            <div className="text-muted"> <i class="fas fa-times fa-lg"></i> &nbsp; Monthly Status Reports</div>
+                            <div className="prycing">
+                                <button type="button" class="btn btn-primary">BUTTON</button>
+                            </div>
+                        </div>
+                    </div>
+                
+
+
+
+                <div className="col-md-4 items">
+                    <div className="box-inside">
+                        
+                            <h5 className="text-center text-muted" >PRO</h5>
+                            <h6 className="text-center"><span className="amount">$49</span>/month</h6>
+                            <hr />
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;<strong>Unlimited Users</strong></div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;150GB Storage </div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Unlimited Public Projects</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Community Access</div>
+                            <div cl> <i class="fas fa-check fa-lg"></i> &nbsp;Unlimited Private Projects</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Dedicated Phone Support</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;<strong>Unlimited</strong> Free Subdomain</div>
+                            <div> <i class="fas fa-check fa-lg"></i> &nbsp;Monthly Status Reports</div>
+                            <div className="prycing">
+                                <button type="button" class="btn btn-primary">BUTTON</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+        </div>
+    )
+}
+
+
+export default Card;
